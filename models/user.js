694480baab136f8038f0747bff9f3af6b18d2141
zainab-16-marker <zaynp01602@gmail.com>
2026-08-20 14:24:00 +0300

    create he models

--- models/user.js
+++ models/user.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+name:String,
+price:Number,
+quantity:Number,
+  category:{
+     type:String,
+     enum: ['skincare', 'makeup','haircare','bodycare','tools&accessories'],
+  },
+
+
+})
+
+
+const shippmentSchema = new mongoose.Schema({
+user:[userSchema],
+product:[productSchema],
+  status:{
+     type:String,
+     enum: ['pending', 'shipped','deliverd'],
+shippmentAdress:String,
+shippmentDate:Date,
+  },
+
+
+})
 // create the schema
 const userSchema = new mongoose.Schema({
   username: {
@@ -11,7 +36,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  role:{
+     type:String,
+     enum: ['admin', 'customer'],
+  },
 });
+
 // initial the model
 
 const User = mongoose.model('User', userSchema);
